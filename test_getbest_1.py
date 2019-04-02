2b3aaf82f0131eb26c8778e5b1aa110c91112e07
Blake Denham <blake.denham@gmail.com>
2019-04-02 13:41:16 +0200

    d

--- test_getbest_1.py
+++ test_getbest_1.py
@@ -6,12 +6,13 @@ file_path = os.path.dirname(__file__)
 
 class Test(unittest.TestCase):
     def setup(self):
-        self.test_data =os.path.join(file_path, "bestdat0.csv")
+        self.test_data =open(os.path.join(file_path, "bestdat0.csv"))
 
     def test(self):
+        f = self.test_data
         num_col,mark_col = getbest.getCols(self.test_data)
-        self.assertEqual(num_col[1], 160001)
-        self.assertEqual(mark_col[1], 72) 
+        best_idx, best = findTop(,num_col,mark_col)
+        self.assertEqual(best, 90)
 
 
 if __name__ == '__main__':
